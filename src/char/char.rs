0d217b3ced0e45f74feaed14ce02e1344bebb7e2
Ismael Gomes Moreira <ismaelmoreirakt@gmail.com>
2025-03-12 14:01:36 +0000

    feat: Added the assert_char_is_alphanumeric function that checks if the character is alphabetic or numeric.

--- src/char/char.rs
+++ src/char/char.rs
@@ -7,7 +7,7 @@
 //! @license RTAPT is licensed under the LGPL-2.1-or-later.
 //!
 //! @author Ismael Gomes Moreira <ismaelmoreirakt@gmail.com>
-use crate::char::constants::ALPHA;
+use crate::char::constants::*;
 
 /// Verifies if two `char`s are equal. Panics if they are not.
 ///
@@ -37,6 +37,25 @@ pub fn assert_char_is_alphabetic(a_char: char) {
     if !ALPHA.contains(&a_char) {
         panic!(
             "assert_char_is_alphabetic: '{}' is not an alphabetic character",
+            a_char
+        );
+    }
+}
+
+/// Verifies if a character is alphanumeric. Panics if it is not.
+///
+/// This function checks if a given character is alphanumeric, meaning it is either
+/// a letter or a digit. If the character is neither a letter nor a digit, it panics.
+///
+/// # Arguments
+/// * `a_char` - The character to check.
+///
+/// # Panics
+/// Panics if the character is not alphanumeric.
+pub fn assert_char_is_alphanumeric(a_char: char) {
+    if !ALPHA_NUMERIC.contains(&a_char) {
+        panic!(
+            "assert_char_is_alphanumeric: '{}' is not an alpha numeric character",
             a_char
         );
     }
